ac0a683c523a4d52a2e729b0337490136e59a7dc
Klifford Agujar <forddagujar95@gmail.com>
2020-02-12 23:09:37 -0500

    added a little more detail

--- src/components/Users.js
+++ src/components/Users.js
@@ -5,7 +5,12 @@ export default class Users extends React.Component{
     render() {
         return (
             <Card body>
-                {this.props.user}
+                <div className="input-group mb-2">
+                    <div className="input-group-prepend">
+                        <div className="input-group-text">@</div>
+                        <input disabled type="text" style={{"maxWidth":"10vw"}} defaultValue={this.props.user}></input>
+                    </div>
+                </div>
             </Card>
         )
     }
