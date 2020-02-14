99d80e4ad5d868f324e3ea8a4b0f3cdaf51f0240
Klifford Agujar <forddagujar95@gmail.com>
2020-02-14 01:03:26 -0500

    removed card wrapper

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,17 +1,14 @@
 import React from 'react'
-import Card from 'react-bootstrap/Card'
 
 export default class Users extends React.Component{
     render() {
         return (
-            <Card body>
-                <div className="input-group mb-2">
-                    <div className="input-group-prepend">
-                        <div className="input-group-text">@</div>
-                        <input disabled type="text" style={{"maxWidth":"10vw"}} defaultValue={this.props.user}></input>
-                    </div>
+            <div className="input-group mb-2">
+                <div className="input-group-prepend">
+                    <div className="input-group-text">@</div>
+                    <input disabled type="text" style={{"maxWidth":"10vw"}} defaultValue={this.props.user}></input>
                 </div>
-            </Card>
+            </div>
         )
     }
 }
