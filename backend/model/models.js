ab1d4dd84f7dc3d7ca966744c0919c0a6641e412
Klifford Agujar <forddagujar95@gmail.com>
2020-02-14 19:05:50 -0500

    added Events model

--- backend/model/models.js
+++ backend/model/models.js
@@ -13,11 +13,37 @@ let Message = new Schema({
     timestamp: {
         type: Date,
         default: Date.now
+    },
+    room: {
+        type: String,
+        required: true
+    }
+})
+
+let Events = new Schema({
+    type: {
+        type:String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    event: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: String,
+        required: true
     }
 })
 
 module.exports = {
     messageModel() {
         return mongoose.model('Message', Message)
+    },
+    eventModel() {
+        return mongoose.model('Events', Events)
     }
 }
